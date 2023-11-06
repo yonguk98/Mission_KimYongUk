731799ee819015e676f204c4f6c3bcb12e75d65c
yonguk <kimyu9827@naver.com>
2023-11-06 13:16:02 +0900

    8단계 완료

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -17,26 +17,27 @@ public class Main {
             String cmd = scanner.next();
 
             String[] cmdArr = cmd.split("\\?");
+            String action = cmdArr[0];
 
-            if(cmd.contains("종료")){ // 1단계
+            if(action.contains("종료")){ // 1단계
                 System.exit(0);
             }
-            if(cmd.contains("등록")){ // 2단계
-                System.out.print("명언:");
+            if(action.contains("등록")){ // 2단계
+                System.out.print("명언 : ");
                 String content = scanner.next();
-                System.out.print("작가");
+                System.out.print("작가 : ");
                 String writer = scanner.next();
                 System.out.println(sequence + "번 명언이 등록되었습니다."); // 3단계
 
                 map.put(sequence,new Say(sequence,content,writer));
                 sequence++; // 4단계
             }
-            if(cmd.contains("목록")){ // 5단계
+            if(action.contains("목록")){ // 5단계
                 for (Say say : map.values()) {
                     System.out.println(say.getId()+" / "+say.getWriter()+" / "+say.getContent());
                 }
             }
-            if(cmd.contains("삭제")){ // 6,7 단계
+            if(action.contains("삭제")){ // 6,7 단계
                 Integer id = null;
                 try {
                     id = Integer.parseInt(cmdArr[1].split("id=",-1)[1].trim());
@@ -52,6 +53,27 @@ public class Main {
                     }else{
                         System.out.println(id + "번 명언은 존재하지 않습니다.");
                     }
+                }
+            }
+            if(action.equals("수정")){ // 8단계
+                Integer id = null;
+                try {
+                    id = Integer.parseInt(cmdArr[1].split("id=",-1)[1].trim());
+                }catch (Exception e){
+                    System.out.println("id parsing error");
+                }
+                if(map.containsKey(id)){
+                    Say say = map.get(id);
+                    System.out.println("명언(기존)" + say.getContent() );
+                    System.out.print("명언 : ");
+                    String content = scanner.next();
+                    System.out.println("작가(기존)" + say.getWriter());
+                    System.out.print("작가 : ");
+                    String writer = scanner.next();
+                    say.setContent(content);
+                    say.setWriter(writer);
+                }else {
+                    System.out.println(id + "번 명언은 존재하지 않습니다.");
                 }
             }
 
