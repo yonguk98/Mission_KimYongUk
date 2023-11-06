f22c5db21186e332b3050123b7fa1d62a11a3cf6
yonguk <kimyu9827@naver.com>
2023-11-06 11:43:34 +0900

    7단계 커밋

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,7 +1,53 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sequence = 1;
+        Map<Integer,Say> map = new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===명언 앱===");
+        while(true){
+            System.out.print("명령) ");
+            String cmd = scanner.next();
+
+            if(cmd.contains("종료")){ // 1단계
+                System.exit(0);
+            }
+            if(cmd.contains("등록")){ // 2단계
+                System.out.print("명언:");
+                String content = scanner.next();
+                System.out.print("작가");
+                String writer = scanner.next();
+                System.out.println(sequence + "번 명언이 등록되었습니다."); // 3단계
+
+                map.put(sequence,new Say(sequence,content,writer));
+                sequence++; // 4단계
+            }
+            if(cmd.contains("목록")){ // 5단계
+                for (Say say : map.values()) {
+                    System.out.println(say.getId()+" / "+say.getWriter()+" / "+say.getContent());
+                }
+            }
+            if(cmd.contains("삭제")){ // 6,7 단계
+                String[] cmdBits = cmd.split("\\?");
+                Integer id = Integer.parseInt(cmdBits[1].split("id=",-1)[1].trim());
+
+                if(!id.equals(null)){
+                    if(map.containsKey(id)) {
+                        map.remove(id);
+                        System.out.println(id + "번 명언이 삭제되었습니다.");
+                    }else{
+                        System.out.println(id + "번 명언은 존재하지 않습니다.");
+                    }
+                }
+            }
+
+        }
     }
 }
---
+++ src/main/java/com/ll/Say.java
@@ -0,0 +1,17 @@
+package com.ll;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Say {
+    private int id;
+    private String content;
+    private String writer;
+
+    public Say(int id, String content, String writer) {
+        this.id = id;
+        this.content = content;
+        this.writer = writer;
+    }
+}
